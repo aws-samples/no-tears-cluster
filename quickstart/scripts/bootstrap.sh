96b50443a2c1198fe097b5a4aa366e5ba7226765
Sean Smith <seaam@amazon.com>
2020-04-01 20:37:59 -0700

    Added Makefile
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- quickstart/scripts/bootstrap.sh
+++ quickstart/scripts/bootstrap.sh
@@ -1,3 +1,48 @@
 #!/usr/bin/env bash
 
-pip-3.6 install aws-parallelcluster -U --user
+
+pip-3.6 --disable-pip-version-check install aws-parallelcluster -U --user
+
+
+AWS_REGION=us-east-1
+KEY_NAME=test
+
+cat >> ~/.parallelcluster/config <<EOF
+[global]
+cluster_template = covid
+update_check = true
+sanity_check = true
+
+[aws]
+aws_region_name = ${AWS_REGION}
+
+[aliases]
+ssh = ssh {CFN_USER}@{MASTER_IP} {ARGS}
+
+[cluster covid]
+key_name = ${KEY_NAME}
+base_os = ubuntu1804
+scheduler = slurm
+master_instance_type = c5n.18xlarge
+compute_instance_type = c5n.18xlarge
+vpc_settings = public-private
+fsx_settings = fsx
+disable_hyperthreading = true
+dcv_settings = dcv
+post_install_script =
+
+[fsx fsx]
+shared_dir = /fsx
+storage_capacity = 1200
+
+[dcv dcv]
+enable = master
+port = 8443
+access_from = 0.0.0.0/0
+
+[vpc public-private]
+vpc_id = $(aws --region ${AWS_REGION} cloudformation  describe-stacks --stack-name VPCStack --query "Stacks[0].Outputs[?OutputKey=='VPCID'].OutputValue" --output=text)
+master_subnet_id = $(aws --region ${AWS_REGION} cloudformation  describe-stacks --stack-name VPCStack --query "Stacks[0].Outputs[?OutputKey=='PublicSubnet1ID'].OutputValue" --output=text)
+compute_subnet_id = $(aws --region ${AWS_REGION} cloudformation  describe-stacks --stack-name VPCStack --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet1ID'].OutputValue" --output=text)
+
+EOF
