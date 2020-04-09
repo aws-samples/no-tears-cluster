7b8491e55e418a464a7ea689f5ba87840448df53
Sean Smith <seaam@amazon.com>
2020-04-08 23:52:17 -0700

    Remove quickstart directory and re-organize
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- README.md
+++ README.md
@@ -1,14 +1,53 @@
-# Covid-19 Rapid Cluster
+# Setup Cloud9, IAM, pcluster environment
 
-This repo rapidly deploys a cloudformation stack that creates a Cloud9 instance, VPC, subnets, IAM roles ect.
+The first step is installing node.js, this can be done easily with Homebrew. After that completes, install aws-cdk:
 
-To get started, run:
-
-```bash
-cd quickstart/
+```
+$ brew install node
+$ npm install -g aws-cdk
 ```
 
-That's it!
+Now you can activate the python virtualenv and install the python dependencies:
 
-## Accout  independent Version
-To build an account independent version use ./upload.sh This will upload all assets to $s3_bucket and make sure all references in cfn.yaml point to the correct resources
+```
+$ source .env/bin/activate
+$ pip install -r requirements.txt
+```
+
+At this point, it's time to setup CDK, the following needs to be done once in each account:
+
+```
+$ cdk bootstrap
+```
+
+And finally, deploy the app:
+
+```
+$ cdk deploy
+```
+
+I've surely missed a bunch of python dependencies, the format for installing those is:
+
+```
+$ pip install aws-cdk.custom-resources
+```
+
+Once it finishes deploy, you'll get an ouput with a link to the Cloud9 URL. Click on that to quickly see the Cloud9 result:
+
+![image](https://user-images.githubusercontent.com/5545980/78568726-61c20280-77d7-11ea-84a5-bdf0d7a0cb95.png)
+
+## Pro-Tips
+
+Use `cdk synth | less` to see the generated template.
+
+Provide parameters to the stack via `cdk deploy --parameters pcluster:KEY=VALUE`.
+
+## Useful commands
+
+ * `cdk ls`          list all stacks in the app
+ * `cdk synth`       emits the synthesized CloudFormation template
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk docs`        open CDK documentation
+
+Enjoy!
---
+++ upload.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+
+# Run this file to copy all required assets into $s3_bucket as well as ${s3_bucket}-${region}
+# for all regions and make cfn.yaml reference those assets.
+
+# Output cfn.yaml : Can be used on a new clean account by a different user.
+
+s3_bucket=${1:-covid19hpc-quickstart} #8192-stesachs #covid19hpc-bucket-stesachs #covid19hpc-quickstart-161153343288
+
+# Edit cfn.yaml to include correct bucket
+edit_cfn()
+{
+    awk -v s3=${s3_bucket} '1;/Description: S3 bucket for asset/{ print "    Default: \"" s3 "\""}' cfn.yaml > cfn-changed.yaml
+    mv cfn-changed.yaml cfn.yaml
+
+}
+
+upload()
+{
+    # Report id, local path and s3_key for each asset
+    eval "$(python3 report_s3.py ${s3_bucket})"
+
+    for ((i=0; i<${#asset_id[@]}; i++))
+    do
+	# DEBUG echo "${asset_id[$i]} ${asset_key[$i]}"
+	# Edit cfn.yaml to include correct keys
+	awk -v key=${asset_key[$i]}  '1;/Description: S3 key for asset version "'${asset_id[$i]}'"/{ print "    Default: \"" key "||\""}' cfn.yaml > cfn-changed.yaml
+	mv cfn-changed.yaml cfn.yaml
+
+	# Edit to point to correct region
+	sed -i -e "s/Ref: ${asset_bucket_param[$i]}/!FindInMap [RegionMap, !Ref 'AWS::Region', RegionBucket]/g" cfn.yaml
+    done
+}
+
+# Upload lambdas in every part of the world and make cfn.yaml aware of their locations
+upload_lambda_worldwide()
+{
+    # Add newline after Mappings
+    echo "
+
+$(cat cfn.yaml)
+" > cfn.yaml
+
+    for region in $( aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
+    do
+	if ! $(aws s3 ls s3://${s3_bucket}-${region} &> /dev/null)
+	then
+	   aws s3 mb s3://${s3_bucket}-${region} --region ${region}
+	fi
+ 	aws s3 cp --acl public-read --recursive s3://${s3_bucket}/asset s3://${s3_bucket}-${region}/asset/
+
+	echo "    ${region}:
+      RegionBucket: ${s3_bucket}-${region}
+$(cat cfn.yaml)
+" > cfn.yaml
+    done
+
+    echo "Mappings: 
+  RegionMap:
+$(cat cfn.yaml)
+"  > cfn.yaml
+
+}
+
+
+#cdk synthesize > cfn.yaml
+#edit_cfn
+#upload
+upload_lambda_worldwide
+
+echo "Use cfn.yaml for CloudFormation"
